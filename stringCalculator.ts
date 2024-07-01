0da6163903e9b2ad3e6a97d224ace23a2039e66d
Prathmesh Agrawal <prathmesh.agrawal@justdial.com>
2024-07-01 21:53:17 +0530

    step 4: handles condtion for line change in beginig of string

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -3,8 +3,13 @@ function add(numbers: string): number {
         return 0;
     }
 
-    const delimiter = /,|\n/;
-    const numberArray = numbers.split(delimiter).map(num => parseInt(num, 10));
+    let delimiter = /,|\n/;
+    if (numbers.startsWith("//")) {
+        const delimiterEndIndex = numbers.indexOf('\n');
+        delimiter = new RegExp(numbers.substring(2, delimiterEndIndex));
+        numbers = numbers.substring(delimiterEndIndex + 1);
+    }
 
+    const numberArray = numbers.split(delimiter).map(num => parseInt(num, 10));
     return numberArray.reduce((sum, num) => sum + num, 0);
 }

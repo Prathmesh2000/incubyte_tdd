5eb9606736687f6fdeb4e142cbc971e2184085d8
Prathmesh Agrawal <prathmesh.agrawal@justdial.com>
2024-07-01 22:01:17 +0530

    step 5: add condtion to return msg in case of negative

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -1,15 +1,21 @@
-function add(numbers: string): number {
+function add(numbers: string): number | string {
     if (numbers === "") {
         return 0;
     }
 
     let delimiter = /,|\n/;
-    if (numbers.startsWith("//")) {
-        const delimiterEndIndex = numbers.indexOf('\n');
-        delimiter = new RegExp(numbers.substring(2, delimiterEndIndex));
-        numbers = numbers.substring(delimiterEndIndex + 1);
+    if (numbers.indexOf("//") === 0) {
+        const delimiterLineEnd = numbers.indexOf('\n');
+        delimiter = new RegExp(numbers.substring(2, delimiterLineEnd));
+        numbers = numbers.substring(delimiterLineEnd + 1);
     }
 
     const numberArray = numbers.split(delimiter).map(num => parseInt(num, 10));
+    const negativeNumbers = numberArray.filter(num => num < 0);
+
+    if (negativeNumbers.length > 0) {
+        return `negative numbers not allowed ${negativeNumbers.join(', ')}`;
+    }
+
     return numberArray.reduce((sum, num) => sum + num, 0);
 }
